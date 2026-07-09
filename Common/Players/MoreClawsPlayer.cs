94f44f7d51a6227723f4697a98f71a34b0d0125d
ppkr36 <dimchick10@gmail.com>
2026-07-09 04:49:17 +0300

    added copper claws

---
+++ Common/Players/MoreClawsPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ClawProgression.Content.Claws;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ClawProgression.Common.Players
+{
+    public class MoreClawsPlayer : ModPlayer
+    {
+        public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+        {
+            if (mediumCoreDeath)
+            {
+                yield break;
+            }
+
+            Item copperClaw = new Item();
+            copperClaw.SetDefaults(ModContent.ItemType<CopperClaw>());
+
+            yield return copperClaw;
+        }
+    }
+}
--- Content/Claws/CopperClaw.cs
+++ Content/Claws/CopperClaw.cs
@@ -6,8 +6,32 @@ using Terraria.ModLoader;
 
 namespace ClawProgression.Content.Claws
 {
-    internal class CopperClaw : ModItem
+    public class CopperClaw : ModItem
     {
-
+        public override void SetDefaults() {
+            Item.DamageType = DamageClass.Melee;
+            Item.useTime = 10;
+            Item.useAnimation = 12;
+            Item.damage = 2;
+            Item.pick = 35;
+            Item.width = 32;
+            Item.height = 32;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.pick = 35;
+            Item.axe = 5;
+            Item.tileBoost = -2;
+            Item.UseSound = SoundID.Item1;
+            Item.value = Item.sellPrice(copper: 50);
+            Item.rare = ItemRarityID.White;
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.CopperBar, 6);
+            recipe.AddRecipeGroup(RecipeGroupID.Wood, 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.Register();
+        }
     }
 }
